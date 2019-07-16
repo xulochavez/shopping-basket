7f608ef90b49252d545648f2206d546a8d07026d
Pablo <pablo@pablos-macbook.home>
2019-07-16 14:00:25 +0100

    another attempt to get a running flask server

--- basket/__main__.py
+++ basket/__main__.py
@@ -15,9 +15,13 @@ import json
 import sys
 import time
 
+from flask import Flask, make_response, jsonify
+
 from basket import product
 from basket import promotion
 from basket import basket
+from basket.service_config import Config
+from basket import service_routes
 
 
 class SimpleLogger:
@@ -181,6 +185,27 @@ def main(argv=None):
     else:
         print('(No offers available)')
     print(f'Total: £{shopping_basket.total/100:.2f}')
+
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+
+    if test_config:
+        app.config.from_object(test_config)
+    else:
+        app.config.from_object(Config)
+
+    app.register_blueprint(service_routes.bp)
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return make_response(jsonify({'error': 'Internal Server error'}), 500)
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return make_response(jsonify({'error': 'Not found'}), 404)
+
+    return app
 
 
 if __name__ == '__main__':  # pragma: no cover
